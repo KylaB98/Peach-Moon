6eecae2af8a11b2de19bd69f26401632cf2581ee
Kyla Wootson <kylawootson@MacBook-Pro.local>
2022-07-26 23:55:41 -0400

    Please work

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,20 @@ import Product from './Pages/Product';
 import ProductList from './Pages/ProductList';
 import Cart from './Pages/Cart';
 import ContactForm from './Pages/ContactForm';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { Router, Routes, Route, Link } from 'react-router-dom';
 import AppRouter from './AppRouter';
 
 
 const App = () => {
   return (
-    <AppRouter />
+    <Router>
+    <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/contact" element={<ContactForm />} />
+        <Route path="/cart" element={<Cart />} />
+    </Routes>
+</Router>
   )
 }
 
